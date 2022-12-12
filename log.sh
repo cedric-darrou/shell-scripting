57318985d3f342618bb28e8b4985519e93b30f3f
cedric-darrou <c.darrou@gmx.com>
2022-12-12 06:41:31 -0500

    Update logsh
    Create file path is doesn't exist

--- log.sh
+++ log.sh
@@ -1,5 +1,15 @@
 #! /bin/zsh
+dir="var/log/exercice/recent_logs"
 
+# Check if dir exist. If it doesn't existe, create path
+if [ -d "$dir" ]; then
+	echo "File already exist"
+else
+	mkdir -p $dir
+	echo "Create dir for logs"
+fi
+
+# Create n file tmp from /dev/random
 for (( i=0; i<$1; i++ ))
 do
 	log=$(head -c10 /dev/random)
