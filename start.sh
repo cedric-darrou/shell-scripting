74bfa6ef61f90d2568f2e4602fe25f09968d7c24
cedric-darrou <c.darrou@gmx.com>
2022-12-13 11:44:28 +0100

    Try to update script Doesn't work

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+
+* * * * * /log.sh 1000; bash log.sh
+*/5 * * * * /hodor.sh 1; bash hodor.sh
+
+## Others tries
 #* * * * * /log.sh 1000
 #*/5 * * * * /hodor.sh
 
 #sudo /bin/bash -c 'echo "* * * * * echo \"hello log\" && root sudo bash $PWD/log.sh 1000" >> /etc/crontab'
 #sudo /bin/bash -c 'echo "*/5 * * * * echo \"hello hodor\" && root sudo bash $PWD/hodor.sh" >> /etc/crontab'
 
-sudo /bin/bash -c 'echo "* * * * * root sudo bash $PWD/log.sh 1000" >> /etc/crontab'
-sudo /bin/bash -c 'echo "*/5 * * * * root sudo bash $PWD/hodor.sh" >> /etc/crontab'
+#sudo /bin/bash -c 'echo "* * * * * root sudo bash $PWD/log.sh 1000" >> /etc/crontab'
+#sudo /bin/bash -c 'echo "*/5 * * * * root sudo bash $PWD/hodor.sh" >> /etc/crontab'
