d543895c52c1e0be1b43e08dcbb96b21820d3a4b
cedric-darrou <c.darrou@gmx.com>
2022-12-13 11:17:55 +0100

    update start.sh sould be work and delete var folder

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 #* * * * * /log.sh 1000
 #*/5 * * * * /hodor.sh
 
-sudo /bin/bash -c 'echo "* * * * * echo \"hello log\" && root $PWD/log.sh 1000" >> /etc/crontab'
-sudo /bin/bash -c 'echo "*/5 * * * * echo \"hello hodor\" && root $PWD/hodor.sh" >> /etc/crontab'
+#sudo /bin/bash -c 'echo "* * * * * echo \"hello log\" && root sudo bash $PWD/log.sh 1000" >> /etc/crontab'
+#sudo /bin/bash -c 'echo "*/5 * * * * echo \"hello hodor\" && root sudo bash $PWD/hodor.sh" >> /etc/crontab'
+
+sudo /bin/bash -c 'echo "* * * * * root sudo bash $PWD/log.sh 1000" >> /etc/crontab'
+sudo /bin/bash -c 'echo "*/5 * * * * root sudo bash $PWD/hodor.sh" >> /etc/crontab'
