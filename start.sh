f48b77a1129b34a62f0ff48c9931c473eb770f5b
cedric-darrou <c.darrou@gmx.com>
2022-12-13 11:01:47 +0100

    Update start.sh Doesn't work...

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-crontab -e * * * * * log.sh
-crontab -e */5 * * * * hodor.sh
+#* * * * * /log.sh 1000
+#*/5 * * * * /hodor.sh
 
-
+sudo /bin/bash -c 'echo "* * * * * echo \"hello log\" && root $PWD/log.sh 1000" >> /etc/crontab'
+sudo /bin/bash -c 'echo "*/5 * * * * echo \"hello hodor\" && root $PWD/hodor.sh" >> /etc/crontab'
